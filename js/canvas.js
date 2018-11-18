ab60db1eca6ade36aadfa461bb92af3f6570a459
phoenix33 <phoenix33g@gmail.com>
2018-11-17 19:56:57 -0500

    First Push to github

---
+++ js/canvas.js
@@ -0,0 +1,6 @@
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global window, document */
+
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
